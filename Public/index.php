1b25d6b284c94510934c044f20d8cbe20ae90ce5
Felipe Reno Oliveira Pisa <felipe@note.(none)>
2011-11-21 23:40:11 -0200

    Arrumando GRAPH_DATA_DIR

--- Public/index.php
+++ Public/index.php
@@ -6,7 +6,7 @@ ini_set('display_startup_errors', 'On');
 define('LIBRARY_DIR', '/home/felipe/TG-II/Library');
 set_include_path(get_include_path() . PATH_SEPARATOR . LIBRARY_DIR);
 
-define('DATA_DIR', './home/felipe/TG-II/Data');
+define('DATA_DIR', '/home/felipe/TG-II/Data');
 define('GRAPH_DATA_DIR', DATA_DIR . DIRECTORY_SEPARATOR . 'Graphs');
 define('RESULT_DATA_DIR', DATA_DIR . DIRECTORY_SEPARATOR . 'Results');
 
