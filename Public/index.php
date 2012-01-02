4e316720cb1bacf48cc746b984e316208dc66e73
Felipe Reno Oliveira Pisa <felipe@manga.(none)>
2012-01-02 13:38:07 -0200

    Doc constantes de configuracao e melhor ordem de defines

--- Public/index.php
+++ Public/index.php
@@ -4,9 +4,10 @@ ini_set('display_errors', 'On');
 ini_set('display_startup_errors', 'On');
 
 define('LIBRARY_DIR', '/home/felipe/TG-II/Library');
+define('DATA_DIR', '/home/felipe/TG-II/Data');
+
 set_include_path(get_include_path() . PATH_SEPARATOR . LIBRARY_DIR);
 
-define('DATA_DIR', '/home/felipe/TG-II/Data');
 define('GRAPH_DATA_DIR', DATA_DIR . DIRECTORY_SEPARATOR . 'Graphs');
 define('RESULT_DATA_DIR', DATA_DIR . DIRECTORY_SEPARATOR . 'Results');
 
@@ -19,3 +20,9 @@ $servico = new RestService();
 $servico->handle();
 
 $servico->SendResponse(TRUE);
+
+/**
+LIBRARY_DIR: caminho absoluto do diretório que contém a biblioteca do sistema.
+DATA_DIR: caminho absoluto do diretório que contém os arquivos de grafo (DATA_DIR/Graphs) e de resultado (DATA_DIR/Results).
+*/
+
